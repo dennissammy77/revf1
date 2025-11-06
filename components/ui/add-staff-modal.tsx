5b30f34f326effafdacf9ffa8cbb00932d7393e3
dennissammy77 <dennissammy77@gmail.com>
2025-11-06 11:07:24 +0300

    style: fix

--- components/ui/add-staff-modal.tsx
+++ components/ui/add-staff-modal.tsx
@@ -9,6 +9,7 @@ import {
   Dialog,
   Text,
   Portal,
+  HStack,
 } from "@chakra-ui/react";
 import { addStaffAction } from "@/app/actions/user";
 import { useRouter } from "next/navigation";
@@ -42,13 +43,13 @@ export default function AddStaffDialog({ isOpen, onOpenChange }: AddStaffDialogP
 
   return (
     <>
-      <Dialog.Root open={isOpen} onOpenChange={(e) => onOpenChange(!!e.open)} >
+      <Dialog.Root open={isOpen} onOpenChange={(e) => onOpenChange(!!e.open)}>
         <Portal>
           <Dialog.Backdrop />
           <Dialog.Positioner>
             <Dialog.Content>
               <Dialog.Header>
-                <Heading size="lg">Add staff</Heading>
+                <Heading size="lg" mx='6' mt='4' mb='2'>Add staff</Heading>
               </Dialog.Header>
               <Dialog.Body p={6}>
                 <form
@@ -59,19 +60,26 @@ export default function AddStaffDialog({ isOpen, onOpenChange }: AddStaffDialogP
                   }
                 >
                   <Flex direction="column" gap={3}>
-                    <Input name="name" placeholder="Name (optional)" />
+                    <Text>Name</Text>
+                    <Input name="name" placeholder="Name" />
+                    <Text>Email</Text>
                     <Input name="email" placeholder="Email" type="email" required />
+                    <Text>phone</Text>
                     <Input name="phone" placeholder="Phone (10 digits)" required />
+                    <Text>Password</Text>
                     <Input name="password" placeholder="Password" type="password" required />
-                    <Button type="submit" backgroundColor={"black"} px='2' fontWeight={"bold"} color={"white"} disabled={isPending}>
-                      {isPending ? "Adding..." : "Add staff"}
-                    </Button>
+                    <HStack>
+                      <Button type="submit" backgroundColor={"black"} px='2' fontWeight={"bold"} color={"white"} disabled={isPending}>
+                        {isPending ? "Adding..." : "Add staff"}
+                      </Button>
+                      <Button variant="ghost" backgroundColor={"white"} borderColor={"black"} borderWidth={1} px='2' fontWeight={"bold"} onClick={onClose}>
+                        Cancel
+                      </Button>
+
+                    </HStack>
                   </Flex>
                 </form>
               </Dialog.Body>
-              <Dialog.Footer>
-                <Button variant="ghost" onClick={onClose}>Cancel</Button>
-              </Dialog.Footer>
             </Dialog.Content>
           </Dialog.Positioner>
         </Portal>
